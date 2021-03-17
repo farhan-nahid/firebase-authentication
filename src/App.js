375c387caffa9a572bdd1a820626acbc38045d36
farhan-nahid <nahidahmed9933@gmail.com>
2021-03-17 06:34:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import React from 'react';
+import { firebaseConfig } from './firebase.config';
+//import image from './images/authentication.svg';
+
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   </>
   );
 }
 
