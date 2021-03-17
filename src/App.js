586fc3b2c2aea0aa085a1e94fdb4ce1aad70f904
farhan-nahid <nahidahmed9933@gmail.com>
2021-03-18 00:27:01 +0600

    just fb, gitgub & google

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import firebase from "firebase/app";
 import "firebase/auth";
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import './App.css';
 import { firebaseConfig } from './firebase.config';
@@ -17,7 +17,91 @@ if (!firebase.apps.length) {
 }
 
 function App() {
+const [loggedInUser, setLoggedInUser] = useState({
+  first : "",
+  last : "",
+  address : "",
+  email : "",
+  password : ""
+})
+  
+// google provider function
+const handelGoogleSignIn= () => {
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
 
+  firebase
+  .auth()
+    .signInWithPopup(googleProvider)
+    .then((result) => {
+      const user = result.user;
+      setLoggedInUser(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+  });
+}
+
+
+// facebook provider function
+const handelFacebookSignIn = () => {
+  const facebookProvider = new firebase.auth.FacebookAuthProvider();
+
+  firebase
+  .auth()
+    .signInWithPopup(facebookProvider)
+    .then((result) => {
+      const user = result.user;
+      setLoggedInUser(user);
+   })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+  });
+}
+
+
+// github provider function
+const handelGithubSignIn = () => {
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+
+  firebase
+  .auth()
+    .signInWithPopup(githubProvider)
+    .then((result) => {
+      const user = result.user;
+      setLoggedInUser(user);
+     })
+      .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+  });
+}
+
+// event handel
+const handelBlur = (e) =>{
+  let isFieldValid = true;   
+  if(e.target.name === "email"){
+    isFieldValid = /\S+@\S+\.\S+/.test(e.target.value)
+  }
+  if(e.target.name === "password"){
+    const isPasswordValid = e.target.value.length > 6;
+    const passwordHasNumber =  /\d{1}/.test(e.target.value)
+    isFieldValid = isPasswordValid && passwordHasNumber
+  }
+  if(isFieldValid){
+    const newUserInfo ={...loggedInUser}
+    newUserInfo[e.target.name] = e.target.value
+    setLoggedInUser(newUserInfo)
+  }
+}
+
+const handelSubmit = () =>{
+
+}
   return (
         <>
            <Navbar bg="light" expand="lg" >
@@ -25,7 +109,8 @@ function App() {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                  <Nav.Link href="#home">Log In</Nav.Link>
+                  <Nav.Link href="#home">Sign In</Nav.Link>
+                  <Nav.Link href="#home">Sign Out</Nav.Link>
                   <Nav.Link href="#link">Sign Up</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
@@ -36,26 +121,36 @@ function App() {
                     <img src= {image} alt=".." className="img-fluid"/>
                 </Col>
                 <Col md={5} className="form-input">
-                  <br/> <br/>
-                  <h3 className="text-center">Sign Up hare</h3>
-                    <form action="" >
-                      <input type="text" placeholder="First Name"/> 
-                      <br/> <br/>
-                      <input type="text" placeholder="Last Name"/>
-                      <br/><br/>
-                      <input type="text" placeholder="Address"/>
-                      <br/><br/>
-                      <input type="email" name="" id="" placeholder="Your Email"/>
-                      <br/><br/>
-                      <input type="password" name="" id="" placeholder="Your Password"/>
-                      <br/><br/>
-                      <input type="submit" value="Sign Up as a new User"/>
+                <h6> User Name :  {loggedInUser.displayName}</h6>
+                  <br/>
+                  <h4 className="text-center">Sign Up </h4>
+                    <form onSubmit={handelSubmit} >
+                      <input type="text" name="first" onBlur={handelBlur}  placeholder="First Name" required/> 
+                        <br/> <br/>
+                      <input type="text" name="last" onBlur={handelBlur}  placeholder="Last Name" required/>
+                         <br/><br/>
+                      <input type="text" name="address" onBlur={handelBlur}  placeholder="Address (Optional)"/>
+                        <br/><br/>
+                      <input type="email" name="email" id="" onBlur={handelBlur} placeholder="Your Email" required/>
+                         <br/><br/>
+                      <input type="password" name="password" id="" onBlur={handelBlur}  placeholder="Your Password" required/>
+                        <br/><br/>
+                      <input type="submit" value="Sign Up As New User"/>
                     </form>
-                    <br/> <br/>
+                       <br/> <br/>
                     <h5 className="border-top">Log in With </h5>
-                    <button className="text-primary"><FontAwesomeIcon icon={faFacebook} /></button>
-                    <button className="text-danger mr-3 ml-3"><FontAwesomeIcon icon={faGoogle} /></button>
-                    <button className="text-dark"> <FontAwesomeIcon icon={faGithub} /></button>
+
+                    <button className="text-primary" onClick={handelFacebookSignIn}>
+                      <FontAwesomeIcon icon={faFacebook} />
+                    </button>
+
+                    <button className="text-danger mr-3 ml-3" onClick={handelGoogleSignIn}>
+                      <FontAwesomeIcon icon={faGoogle} />
+                      </button>
+
+                    <button className="text-dark" onClick={handelGithubSignIn}> 
+                    <FontAwesomeIcon icon={faGithub} />
+                    </button>
                     
                 </Col>
             </Row>
